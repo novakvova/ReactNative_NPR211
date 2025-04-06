da98a56a57ccaaba72043e65180a1f7fdc16511b
novakvova <novakvova@gmail>
2025-04-06 09:51:05 +0300

    Add WebShopDbContext

---
+++ WebShopApi/WebShopApi/Data/Entities/Identity/UserEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebShopApi.Data.Entities.Identity
+{
+    public class UserEntity : IdentityUser<long>
+    {
+        [StringLength(255)]
+        public string? Image { get; set; }
+        [StringLength(100)]
+        public string? Lastname { get; set; }
+        [StringLength(100)]
+        public string? Firstname { get; set; }
+        public virtual ICollection<UserRoleEntity>? UserRoles { get; set; }
+    }
+}
---
+++ WebShopApi/WebShopApi/Data/WebShopDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebShopApi.Data.Entities.Identity;
+
+namespace WebShopApi.Data
+{
+    public class WebShopDbContext : IdentityDbContext<UserEntity, RoleEntity, long>
+    {
+        public WebShopDbContext(DbContextOptions<WebShopDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<UserRoleEntity>(ur =>
+            {
+                ur.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(r => r.RoleId)
+                    .IsRequired();
+
+                ur.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(u => u.UserId)
+                    .IsRequired();
+            });
+        }
+    }
+}
