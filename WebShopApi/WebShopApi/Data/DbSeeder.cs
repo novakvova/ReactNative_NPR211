4caff1e3d31801c2cb9b65e915d9d127bb385768
novakvova <novakvova@gmail>
2025-04-06 11:20:16 +0300

    Role seed database

---
+++ WebShopApi/WebShopApi/Data/DbSeeder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebShopApi.Constants;
+using WebShopApi.Data.Entities.Identity;
+
+namespace WebShopApi.Data
+{
+    public static class DbSeeder
+    {
+        public static async Task SeedAsync(this WebApplication wa)
+        {
+            using var scope = wa.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<WebShopDbContext>();
+
+            context.Database.Migrate();
+
+            if (!context.Roles.Any())
+            {
+                var rm = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+                await rm.CreateAsync(new RoleEntity { Name = Roles.Admin });
+                await rm.CreateAsync(new RoleEntity { Name = Roles.User });
+            }
+
+            if (!context.Users.Any())
+            {
+                
+            }
+        }
+    }
+}
--- WebShopApi/WebShopApi/Program.cs
+++ WebShopApi/WebShopApi/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using System;
 using Microsoft.EntityFrameworkCore;
 using WebShopApi.Data;
+using WebShopApi.Data.Entities.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<WebShopDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("WebShopApiConnection")));
+
+
+builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<WebShopDbContext>().AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -27,5 +40,7 @@ app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Shop v1
 app.UseAuthorization();
 
 app.MapControllers();
+
+await app.SeedAsync();
 
 app.Run();
