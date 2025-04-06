e506edef0c033c63118144ec61e5448cdc61b775
novakvova <novakvova@gmail>
2025-04-06 10:04:58 +0300

    Add migration

--- WebShopApi/WebShopApi/Program.cs
+++ WebShopApi/WebShopApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebShopApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<WebShopDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("WebShopApiConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
