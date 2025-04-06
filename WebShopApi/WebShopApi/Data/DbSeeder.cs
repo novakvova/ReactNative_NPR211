73763ce5082e38da32aa99f55d5e6498db05b9d7
novakvova <novakvova@gmail>
2025-04-06 12:06:43 +0300

    Seed data is good. Upload images the best

--- WebShopApi/WebShopApi/Data/DbSeeder.cs
+++ WebShopApi/WebShopApi/Data/DbSeeder.cs
@@ -1,29 +1,68 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using WebShopApi.Abstract;
 using WebShopApi.Constants;
 using WebShopApi.Data.Entities.Identity;
+using WebShopApi.Models.Seeder;
 
 namespace WebShopApi.Data
 {
     public static class DbSeeder
     {
-        public static async Task SeedAsync(this WebApplication wa)
+        public static async Task SeedAsync(this WebApplication webApplication)
         {
-            using var scope = wa.Services.CreateScope();
+            using var scope = webApplication.Services.CreateScope();
             var context = scope.ServiceProvider.GetRequiredService<WebShopDbContext>();
 
             context.Database.Migrate();
 
             if (!context.Roles.Any())
             {
-                var rm = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
-                await rm.CreateAsync(new RoleEntity { Name = Roles.Admin });
-                await rm.CreateAsync(new RoleEntity { Name = Roles.User });
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
+                await roleManager.CreateAsync(new RoleEntity { Name = Roles.Admin });
+                await roleManager.CreateAsync(new RoleEntity { Name = Roles.User });
             }
 
             if (!context.Users.Any())
             {
-                
+                var imageService = scope.ServiceProvider.GetRequiredService<IImageService>();
+
+                var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "JsonData", "Users.json");
+                if (File.Exists(jsonFile))
+                {
+                    var jsonData = File.ReadAllText(jsonFile, Encoding.UTF8);
+                    try
+                    {
+                        var users = JsonConvert.DeserializeObject<List<SeederUserModel>>(jsonData)
+                            ?? throw new JsonException();
+                        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+                        foreach (var user in users)
+                        {
+                            var newUser = new UserEntity
+                            {
+                                UserName = user.Email,
+                                Email = user.Email,
+                                PhoneNumber = user.PhoneNumber,
+                                Firstname = user.Firstname,
+                                Lastname = user.Lastname,
+                                Image = await imageService.SaveImageFromUrlAsync(user.Image)
+                            };
+                            var result = await userManager.CreateAsync(newUser, user.Password);
+                            if (result.Succeeded)
+                                await userManager.AddToRoleAsync(newUser, user.Role);
+                            else
+                                Console.WriteLine($"--Error create user \"{user.Email}\"--");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("---Error deserialize Users.json---{0}", ex.Message);
+                    }
+                    
+                }
+                else Console.WriteLine("----Error Read json file Users.json----");
             }
         }
     }
--- WebShopApi/WebShopApi/Program.cs
+++ WebShopApi/WebShopApi/Program.cs
@@ -3,6 +3,9 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using WebShopApi.Data;
 using WebShopApi.Data.Entities.Identity;
+using Microsoft.Extensions.FileProviders;
+using WebShopApi.Abstract;
+using WebShopApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +24,8 @@ builder.Services.AddIdentity<UserEntity, RoleEntity>(options =>
     options.Password.RequireLowercase = false;
 }).AddEntityFrameworkStores<WebShopDbContext>().AddDefaultTokenProviders();
 
+builder.Services.AddScoped<IImageService, ImageService>();
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
@@ -36,6 +41,16 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Shop v1"));
+
+string imagesDirPath = Path.Combine(Directory.GetCurrentDirectory(), builder.Configuration["ImagesDir"]);
+
+Directory.CreateDirectory(imagesDirPath);
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(imagesDirPath),
+    RequestPath = "/images"
+});
 
 app.UseAuthorization();
 
