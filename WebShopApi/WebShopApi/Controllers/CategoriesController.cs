ae12cc406645e16c961ab14ccd0854c76428f04a
novakvova <novakvova@gmail>
2025-05-04 10:17:56 +0300

    View list categories by category

---
+++ WebShopApi/WebShopApi/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebShopApi.Data;
+using WebShopApi.Data.Entities.Identity;
+using WebShopApi.Models.Category;
+
+namespace WebShopApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+[Authorize]
+public class CategoriesController(WebShopDbContext context,
+    UserManager<UserEntity> userManager,
+    IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList()
+    {
+        try
+        {
+            string userName = User.Claims.FirstOrDefault().Value;
+            var user = await userManager.FindByEmailAsync(userName);
+            var list = context.Categories
+                .Where(x => x.UserId == user.Id)
+                .ProjectTo<CategoryItemViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+            return Ok(list);
+        }
+        catch(Exception ex) 
+        {
+            return BadRequest(new { error = ex.Message});
+        }
+    }
+}
---
+++ WebShopApi/WebShopApi/Mapper/CategoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WebShopApi.Data.Entities;
+using WebShopApi.Models.Category;
+
+namespace WebShopApi.Mapper;
+
+public class CategoryMapper : Profile
+{
+    public CategoryMapper()
+    {
+        CreateMap<CategoryEntity, CategoryItemViewModel>();
+    }
+}
