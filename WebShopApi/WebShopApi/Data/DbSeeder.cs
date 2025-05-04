bdc2885ddca2d9c878dee24c553685f9f647415d
novakvova <novakvova@gmail>
2025-05-04 09:54:14 +0300

    Seed user is good

--- WebShopApi/WebShopApi/Data/DbSeeder.cs
+++ WebShopApi/WebShopApi/Data/DbSeeder.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using WebShopApi.Abstract;
 using WebShopApi.Constants;
+using WebShopApi.Data.Entities;
 using WebShopApi.Data.Entities.Identity;
 using WebShopApi.Models.Seeder;
 
@@ -64,6 +65,39 @@ namespace WebShopApi.Data
                 }
                 else Console.WriteLine("----Error Read json file Users.json----");
             }
+
+            if (!context.Categories.Any())
+            {
+                var imageService = scope.ServiceProvider.GetRequiredService<IImageService>();
+                var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "JsonData", "Categories.json");
+                if (File.Exists(jsonFile))
+                {
+                    var jsonData = File.ReadAllText(jsonFile, Encoding.UTF8);
+                    try
+                    {
+                        var categories = JsonConvert.DeserializeObject<List<SeederCategoryModel>>(jsonData)
+                            ?? throw new JsonException();
+                        foreach (var category in categories)
+                        {
+                            var newCategory = new CategoryEntity
+                            {
+                                Name = category.Name,
+                                Description = category.Description,
+                                UserId = category.UserId,
+                                Image = await imageService.SaveImageFromUrlAsync(category.Image)
+                            };
+                            context.Categories.Add(newCategory);
+                            context.SaveChanges();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"--Error parse json--{ex.Message}");
+                    }
+                }
+                else Console.WriteLine($"--Error open file {jsonFile}--");
+            }
+
         }
     }
 }
